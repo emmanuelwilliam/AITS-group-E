16d6e9e5b3512b0e4ff4855239dcc696274ceeb5
Mwizerwa Timothy <mwizerwatimothy204@gmail.com>
2025-02-28 13:16:09 +0300

    configuring django models

--- Backend_django/backendcode/Apps/models.py
+++ Backend_django/backendcode/Apps/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class student(){
+class student(models.Model):
+    name = models.CharField(max_length=100)
+    student_id = models.CharField(max_length=11)
+    reg_no = models.CharField(max_length=20)
     
-}
+    def __str__(self):
+        return self.name
+    
+
+class administrator(models.Model):
+    name = models.CharField(max_length=100)
+    admin_id = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+    
