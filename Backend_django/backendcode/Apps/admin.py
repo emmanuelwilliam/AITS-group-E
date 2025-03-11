51c4000549104d50631b4dbfdc8b2eefb1065ce8
MuhaiseSamson <muhaisesamson@gmail.com>
2025-03-11 09:11:13 +0300

    Prompt for Course units taught

--- Backend_django/backendcode/Apps/admin.py
+++ Backend_django/backendcode/Apps/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Student, Administrator
+from .models import Student, Administrator,CourseUnit
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Administrator)
+admin.site.register(CourseUnit)
