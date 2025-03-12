25f3b34c06769953854af16e51d6bba33cba434c
Mwizerwa Timothy <mwizerwatimothy204@gmail.com>
2025-03-12 15:48:47 +0300

    Use defaultRouter in urls.py

--- Backend_django/backendcode/Apps/admin.py
+++ Backend_django/backendcode/Apps/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Student, Administrator,CourseUnit
+from .models import (
+        Student, 
+         Administrator,
+         CourseUnit,
+         Issue,
+         Notification,
+         Status,
+         LoginHistory,
+)      
+         
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Administrator)
 admin.site.register(CourseUnit)
+admin.site.register(Issue)
+admin.site.register(Notification)
+admin.site.register(Status)
+admin.site.register(LoginHistory)
