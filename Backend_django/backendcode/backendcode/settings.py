39be115f971821ad1c3efe7b4f57336f98f76307
MuhaiseSamson <muhaisesamson@gmail.com>
2025-03-29 23:47:30 +0300

    added exception handler to the rest_framework settings

--- Backend_django/backendcode/backendcode/settings.py
+++ Backend_django/backendcode/backendcode/settings.py
@@ -49,6 +49,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'EXCEPTION_HANDLER': 'myapp.utils.custom_exception_handler'
 }
 
 MIDDLEWARE = [
