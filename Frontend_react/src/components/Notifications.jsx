eadd74a13afae8dea42b116f52f88bc72c6ce772
emmanuelwilliam <sserembaemmanuelsew@gmail.com>
2025-04-01 15:23:48 +0300

    corrected the syntax

--- Frontend_react/src/components/Notifications.jsx
+++ Frontend_react/src/components/Notifications.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import "../styles/notifications.css";
 
 const Notifications = () => {
-  const notifications =[
+  const notifications = [
     "Your issue has been resolved.",
     "New issue reported by Alex.",
     "Reminder: Submit feedback."
-    ];
+  ];
+  
   return (
     <div className="notifications">
       <h2>Notifications</h2>
       <ul>
-        {notifications.map((message, index)>=(
-      <li key={index}>{message}</li>
-                          )}
+        {notifications.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
       </ul>
     </div>
   );
