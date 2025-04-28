f920dbbc97ee9f0cb49a85d65f7a683b099aa047
TIMOTHY-ctrl <mwizerwatimothy204@gmail.com>
2025-04-28 06:47:30 +0300

    Update settings.py deleted comment

--- Backend_django/backendcode/backendcode/settings.py
+++ Backend_django/backendcode/backendcode/settings.py
@@ -241,7 +241,7 @@ LOGGING = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),  # Adjust token lifetime as needed
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),  # Adjust token lifetime 
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
