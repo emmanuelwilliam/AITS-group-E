905b2b68d6fdfbcf9e2e6d7caa670f566a81c631
MuhaiseSamson <muhaisesamson@gmail.com>
2025-05-23 17:11:44 +0300

    updated the vercel.json and vite.config.js

--- Frontend_react/vite.config.js
+++ Frontend_react/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: undefined,
